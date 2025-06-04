0d56ca4c3d766ae8db46f6f4cd027e8d93c4c199
Arina <aqmarinasha@gmail.com>
2025-06-04 23:37:25 +0700

    fix duplicate eye icon

--- src/pages/admin/LoginPage/LoginForm.jsx
+++ src/pages/admin/LoginPage/LoginForm.jsx
@@ -1,11 +1,10 @@
 import { useForm } from "react-hook-form";
 import Input from "@/components/form/input";
 import { Button } from "@/components/ui/button";
-import { Eye, EyeOff } from "lucide-react";
-import { useState } from "react";
+// import { useState } from "react";
 
 export default function LoginForm({ onSubmit, error }) {
-  const [showPassword, setShowPassword] = useState(false);
+  // const [showPassword, setShowPassword] = useState(false);
   const {
     register,
     handleSubmit,
@@ -33,23 +32,13 @@ export default function LoginForm({ onSubmit, error }) {
           label="Password"
           id="password"
           name="password"
-          type={showPassword ? "text" : "password"}
+          // type={showPassword ? "text" : "password"}
+          type="password"
           placeholder="Enter your password"
           {...register("password", { required: "Password is required" })}
           required
           error={errors.password?.message}
         />
-        <div className="absolute inset-y-0 right-0 pr-3 flex items-center mt-8">
-          <Button
-            type="button"
-            variant="ghost"
-            size="icon"
-            className="hover:bg-transparent"
-            onClick={() => setShowPassword((prev) => !prev)}
-          >
-            {showPassword ? <EyeOff size={18} /> : <Eye size={18} />}
-          </Button>
-        </div>
       </div>
 
       <Button
